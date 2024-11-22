7940cb5687acb88317b360d345f42bfbc6aac7d0
sofia-baranetska <sofia.baranetskya@gmail.com>
2024-11-23 00:46:00 +0200

    first commit

---
+++ src/main/java/com/pe/peeureka/PeEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.pe.peeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PeEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeEurekaApplication.class, args);
+	}
+
+}
